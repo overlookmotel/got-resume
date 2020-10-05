fcd11e7fc40b4761df9a314c486d3a37ebf12e19
overlookmotel <theoverlookmotel@gmail.com>
2020-10-05 17:35:48 +0100

    Fix idle timeout [fix]

---
+++ test/idleTimeout.test.js
@@ -0,0 +1,83 @@
+/* --------------------
+ * got-resume module
+ * Tests for idle timeout
+ * ------------------*/
+
+'use strict';
+
+// Modules
+const {Server} = require('http'),
+	gotResume = require('got-resume');
+
+// Constants
+const PORT = 5000,
+	URL = `http://localhost:${PORT}/foo.txt`;
+
+// Init
+require('./support/index.js');
+
+jest.setTimeout(10000);
+
+// Tests
+
+describe('Idle timeout', () => {
+	let server, handle;
+	beforeEach(async () => {
+		server = new Server();
+		server.on('request', (req, res) => handle(req, res));
+		await new Promise((resolve, reject) => {
+			server.once('error', reject);
+			server.listen(PORT, () => {
+				server.off('error', reject);
+				resolve();
+			});
+		});
+	});
+	afterEach(() => new Promise((resolve, reject) => {
+		server.close(err => (err ? reject(err) : resolve()));
+	}));
+
+	it('continues after idle timeout', async () => {
+		const resTxt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+			resLen = resTxt.length,
+			chunkSize = 10;
+
+		handle = (req, res) => {
+			// Serve file in short chunks but stall transmitting each time part-way through
+			const {range} = req.headers;
+			if (!range) {
+				res.setHeader('content-length', resLen);
+				res.write(resTxt.slice(0, chunkSize));
+			} else {
+				const rangeStart = range.match(/^bytes=(\d+)-$/)[1] * 1;
+				res.setHeader('accept-ranges', 'bytes');
+				res.setHeader('content-length', resLen - rangeStart);
+				res.setHeader('content-range', `bytes ${rangeStart}-${resLen - 1}/${resLen}`);
+
+				if (resLen - rangeStart > chunkSize) {
+					res.write(resTxt.slice(rangeStart, rangeStart + chunkSize));
+				} else {
+					res.end(resTxt.slice(rangeStart));
+				}
+			}
+		};
+
+		const stream = gotResume(URL, {timeout: {idle: 100}});
+
+		await new Promise((resolve, reject) => {
+			let out = '';
+			stream.on('data', (data) => {
+				out += data.toString();
+			});
+			stream.on('end', () => {
+				try {
+					expect(out).toBe(resTxt);
+					resolve();
+				} catch (e) {
+					reject(e);
+				}
+			});
+			stream.on('error', reject);
+		});
+	});
+});
