c8eb13b7bda6e77f34dca976be54cb2d39e0c920
overlookmotel <theoverlookmotel@gmail.com>
2023-08-08 18:32:51 +0100

    Fix use of `length` + `offset` options [fix]

--- test/stream.test.js
+++ test/stream.test.js
@@ -24,6 +24,40 @@ describe('`gotResume()`', () => {
 			const txt = await get('short.txt');
 			expect(txt).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 		});
+
+		it('with offset', async () => {
+			const txt = await get('short.txt', {offset: 10});
+			expect(txt).toBe('KLMNOPQRSTUVWXYZ');
+		});
+
+		describe('with length', () => {
+			it('total size of file', async () => {
+				const txt = await get('short.txt', {length: 26});
+				expect(txt).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			});
+
+			it('less than size of file', async () => {
+				const txt = await get('short.txt', {length: 10});
+				expect(txt).toBe('ABCDEFGHIJ');
+			});
+		});
+
+		describe('with length and offset', () => {
+			it('offset 0, length total size of file', async () => {
+				const txt = await get('short.txt', {offset: 0, length: 26});
+				expect(txt).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			});
+
+			it('offset 10, length total size of file', async () => {
+				const txt = await get('short.txt', {offset: 10, length: 26});
+				expect(txt).toBe('KLMNOPQRSTUVWXYZ');
+			});
+
+			it('offset 10, length 20', async () => {
+				const txt = await get('short.txt', {offset: 10, length: 20});
+				expect(txt).toBe('KLMNOPQRST');
+			});
+		});
 	});
 });
 
--- test/toFile.test.js
+++ test/toFile.test.js
@@ -29,6 +29,40 @@ describe('`.toFile()`', () => {
 			const txt = await toFile('short.txt');
 			expect(txt).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 		});
+
+		it('with offset', async () => {
+			const txt = await toFile('short.txt', {offset: 10});
+			expect(txt).toBe('KLMNOPQRSTUVWXYZ');
+		});
+
+		describe('with length', () => {
+			it('total size of file', async () => {
+				const txt = await toFile('short.txt', {length: 26});
+				expect(txt).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			});
+
+			it('less than size of file', async () => {
+				const txt = await toFile('short.txt', {length: 10});
+				expect(txt).toBe('ABCDEFGHIJ');
+			});
+		});
+
+		describe('with length and offset', () => {
+			it('offset 0, length total size of file', async () => {
+				const txt = await toFile('short.txt', {offset: 0, length: 26});
+				expect(txt).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			});
+
+			it('offset 10, length total size of file', async () => {
+				const txt = await toFile('short.txt', {offset: 10, length: 26});
+				expect(txt).toBe('KLMNOPQRSTUVWXYZ');
+			});
+
+			it('offset 10, length 20', async () => {
+				const txt = await toFile('short.txt', {offset: 10, length: 20});
+				expect(txt).toBe('KLMNOPQRST');
+			});
+		});
 	});
 });
 
